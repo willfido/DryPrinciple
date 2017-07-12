c726fb25557f4679be79d58f7815d5f7f6faafd0
willfido <willfido@gmail.com>
2017-07-12 11:43:37 +0100

    Dry Principle added for the repeating code example.

---
+++ DryPrinciple/Example1.RepeatingCode/RepeatCodeDry.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DryPrinciple.Example1.RepeatingCode
+{
+    class RepeatCodeDry
+    {
+        public string GetFruits()
+        {
+            List<string> fruitList = new List<string>();
+
+            fruitList.Add("banana");
+            fruitList.Add("pear");
+            fruitList.Add("apple");
+
+            return GetValues(fruitList);
+        }
+
+        public string GetCarNames()
+        {
+            List<string> carNames = new List<string>();
+
+            carNames.Add("skoda");
+            carNames.Add("ford");
+            carNames.Add("mazda");
+
+            return GetValues(carNames);
+        }
+
+        public string GetValues(List<string> collection)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in collection)
+            {
+                sb.Append(item).Append(",");
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+
+            return sb.ToString();
+        }
+    }
+}
